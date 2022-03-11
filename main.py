0fd23fb9d24059e9f26ae262b29f4104ea74dbcf
sencinia <91294669+sencinia@users.noreply.github.com>
2022-03-10 16:22:01 -0800

    Created main.py
    Completed Day 2 Exercise 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+index_one = two_digit_number[0]
+index_two = two_digit_number[1]
+
+sum = int(index_one) + int(index_two) 
+
+print(index_one + " + " + index_two + " = " + str(sum))
